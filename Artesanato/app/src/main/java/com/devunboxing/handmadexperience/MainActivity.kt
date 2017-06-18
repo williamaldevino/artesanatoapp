3a6e0952b6934da6545a8d536e4b91bf59e928ce
william.santos <william.santos@NT-02792>
2017-06-18 17:09:36 -0300

    substituido MainActivity.java pela versão kotlin

---
+++ Artesanato/app/src/main/java/com/devunboxing/handmadexperience/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.devunboxing.handmadexperience
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by william.santos on 18/06/2017.
+ */
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
